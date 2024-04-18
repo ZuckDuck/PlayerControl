8b98c7c076fad3746469cbc2e61f4ab05c835ab1
zuckduck <zuckd@roverkids.org>
2024-04-18 13:58:29 -0400

    speed

--- PlayerControl/Assets/scripts/PlayerController.cs
+++ PlayerControl/Assets/scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,6 @@ public class PlayerController : MonoBehaviour
     {
         // We'll move the vehicle forward
  
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
